c18cb0f8090d0e74ef3ea727fb2e6d0d79b298fc
tripleT21 <122431943+tripleT21@users.noreply.github.com>
2023-01-11 14:19:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+function Calculator1(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)+parseInt(num2);
+    alert(sum)
+}
+
+function Calculator2(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)-parseInt(num2);
+    alert(sum)
+}
+
+function Calculator3(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)*parseInt(num2);
+    alert(sum)
+}
+
+function Calculator4(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)/parseInt(num2);
+    alert(sum)
+}
+
+function myalert(){
+    alert("Hello")
+}
